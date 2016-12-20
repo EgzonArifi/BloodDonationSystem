102c4c53ffc1e183c7c80f64f6d609642e0df162
egzonarifi <egzonarifi@live.com>
2016-12-20 21:04:35 +0100

    started implementing charts

--- web.BloodDonerManagement/Controllers/HomeController.cs
+++ web.BloodDonerManagement/Controllers/HomeController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace web.BloodDonerManagement.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
@@ -25,6 +27,21 @@ namespace web.BloodDonerManagement.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult CharacterColumn()
+        {
+            ArrayList xValue = new ArrayList();
+            ArrayList yValue = new ArrayList();
+
+            var results = (from c in db.Patient select c);
+            results.ToList().ForEach(rs => xValue.Add(rs.Id));
+            results.ToList().ForEach(rs => yValue.Add(200));
+            new Chart(width: 600, height: 400, theme: ChartTheme.Green)
+                .AddTitle("HEllo from EGzon ARifi")
+                .AddSeries("Default", chartType: "Pie", xValue: xValue, yValues: yValue)
+                .Write("bmp");
+            return null;
         }
     }
 }
