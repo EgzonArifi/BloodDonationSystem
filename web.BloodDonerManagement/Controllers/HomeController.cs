761d6ac85dec97754ebda4d1905742fc0184c733
egzonarifi <egzonarifi@live.com>
2016-12-21 09:17:49 +0100

    shown pie chart on home

--- web.BloodDonerManagement/Controllers/HomeController.cs
+++ web.BloodDonerManagement/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DotNet.Highcharts.Enums;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +15,19 @@ namespace web.BloodDonerManagement.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            DotNet.Highcharts.Highcharts chart = new DotNet.Highcharts.Highcharts("chart")
+         .SetXAxis(new XAxis
+         {
+             Categories = new[] { "0+", "0-", "A+", "A-", "B+", "B-", "AB+", "AB-" }
+         })
+         .SetSeries(new Series
+         {
+             Name = "Raporti i gjakut",
+             Type = ChartTypes.Pie,
+             Data = new Data(new object[] { 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5 })
+         });
+
+            return View(chart);
         }
 
         public ActionResult About()
@@ -28,20 +43,19 @@ namespace web.BloodDonerManagement.Controllers
 
             return View();
         }
+        //public ActionResult CharacterColumn()
+        //{
+        //    ArrayList xValue = new ArrayList();
+        //    ArrayList yValue = new ArrayList();
 
-        public ActionResult CharacterColumn()
-        {
-            ArrayList xValue = new ArrayList();
-            ArrayList yValue = new ArrayList();
-
-            var results = (from c in db.Patient select c);
-            results.ToList().ForEach(rs => xValue.Add(rs.Id));
-            results.ToList().ForEach(rs => yValue.Add(200));
-            new Chart(width: 600, height: 400, theme: ChartTheme.Green)
-                .AddTitle("HEllo from EGzon ARifi")
-                .AddSeries("Default", chartType: "Pie", xValue: xValue, yValues: yValue)
-                .Write("bmp");
-            return null;
-        }
+        //    var results = (from c in db.Patient select c);
+        //    results.ToList().ForEach(rs => xValue.Add(rs.Id));
+        //    results.ToList().ForEach(rs => yValue.Add(200));
+        //    new Chart(width: 600, height: 400, theme: ChartTheme.Green)
+        //        .AddTitle("HEllo from EGzon ARifi")
+        //        .AddSeries("Default", chartType: "Pie", xValue: xValue, yValues: yValue)
+        //        .Write("bmp");
+        //    return null;
+        //}
     }
 }
