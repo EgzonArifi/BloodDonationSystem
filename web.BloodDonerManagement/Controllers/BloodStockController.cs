634ce10e4e7786b39243537e9ef2cb17913102ff
egzonarifi <egzonarifi@live.com>
2016-12-23 00:32:24 +0100

    implemented search patient with autocompletion

--- web.BloodDonerManagement/Controllers/BloodStockController.cs
+++ web.BloodDonerManagement/Controllers/BloodStockController.cs
@@ -30,26 +30,19 @@ namespace web.BloodDonerManagement.Controllers
             return View("Create");
         }
         [HttpPost]
-        public JsonResult PatientSearch(string Prefix)
-        {
+        public JsonResult PatientSearch(string prefix)
+       {
             //Note : you can bind same list from database  
-            List<BloodStockViewModel> ObjList = new List<BloodStockViewModel>()
+            if (prefix == null)
             {
-
-                new BloodStockViewModel {Id=1,Patient="Latur" },
-                new BloodStockViewModel {Id=2,Patient="Mumbai" },
-                new BloodStockViewModel {Id=3,Patient="Pune" },
-                new BloodStockViewModel {Id=4,Patient="Delhi" },
-                new BloodStockViewModel {Id=5,Patient="Dehradun" },
-                new BloodStockViewModel {Id=6,Patient="Noida" },
-                new BloodStockViewModel {Id=7,Patient="New Delhi" }
-
-        };
-            //Searching records from list using LINQ query  
-            var CityName = (from N in ObjList
-                            where N.Patient.StartsWith(Prefix)
-                            select new { N.Patient });
-            return Json(CityName, JsonRequestBehavior.AllowGet);
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
+           List<PatientsViewModel> model = db.Patient.Select(m => new PatientsViewModel
+            {
+                Id = m.Id,
+                Name = m.Name + " " + m.Lastname ,
+            }).Where(c => c.Name.StartsWith(prefix)).ToList();
+            return Json(model, JsonRequestBehavior.AllowGet);
         }
     }
 }
