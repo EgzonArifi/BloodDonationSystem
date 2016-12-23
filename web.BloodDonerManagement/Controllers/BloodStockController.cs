8648b2b4a8a0c04e68acccdc896a64c28ccb6f7a
egzonarifi <egzonarifi@live.com>
2016-12-23 15:31:38 +0100

    implemented blood transfusion

--- web.BloodDonerManagement/Controllers/BloodStockController.cs
+++ web.BloodDonerManagement/Controllers/BloodStockController.cs
@@ -25,9 +25,45 @@ namespace web.BloodDonerManagement.Controllers
             }).ToList();
             return View(model);
         }
-        public ActionResult addOrUpdate()
+        public ActionResult Edit()
         {
             return View("Create");
+        }
+        public ActionResult addOrUpdate(BloodStockViewModel model)
+        {
+
+            var patient = db.Patient.Where(x => x.Id == model.PatientId).FirstOrDefault();
+            var doctor = db.Doctors.Where(x => x.Id == model.DoctorId).FirstOrDefault();
+            if (model.Id == 0)
+                {
+                    db.BloodStock.Add(new BloodStock
+                    {
+                        Patient = patient,
+                        Doctor = doctor,
+                        DonateDate = DateTime.Now,
+                        Comment = model.Comment,
+                        BloodQuantity = model.BloodQuantity,
+
+                    });
+                    db.SaveChanges();
+                    //Session["alertAddNew"] = "True";
+                }
+                else
+                {
+                    var blood = db.BloodStock.Where(x => x.Id == model.Id).FirstOrDefault();
+                    if (blood != null)
+                    {
+                        blood.Patient = model.Donor;
+                        blood.Doctor = model.Doctor;
+                        blood.DonateDate = DateTime.Now;
+                        blood.Comment = model.Comment;
+                        blood.BloodQuantity = model.BloodQuantity;
+                        db.SaveChanges();
+                       // Session["alertEdit"] = "True";
+                    }
+                }
+                return RedirectToAction("Index");
+                //return View("Create");
         }
         [HttpPost]
         public JsonResult PatientSearch(string prefix)
@@ -42,6 +78,21 @@ namespace web.BloodDonerManagement.Controllers
                 Id = m.Id,
                 Name = m.Name + " " + m.Lastname ,
             }).Where(c => c.Name.StartsWith(prefix)).ToList();
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult DoctorSearch(string prefix)
+        {
+            //Note : you can bind same list from database  
+            if (prefix == null)
+            {
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
+            List<DoctorsViewModel> model = db.Doctors.Select(m => new DoctorsViewModel
+            {
+                Id = m.Id,
+                FirstName = m.FirstName + " " + m.LastName,
+            }).Where(c => c.FirstName.StartsWith(prefix)).ToList();
             return Json(model, JsonRequestBehavior.AllowGet);
         }
     }
--- web.BloodDonerManagement/Models/BloodStockViewModel.cs
+++ web.BloodDonerManagement/Models/BloodStockViewModel.cs
@@ -9,11 +9,14 @@ namespace web.BloodDonerManagement.Models
     {
         public int Id { get; set; }
         public string Patient { get; set; }
+        public Patient Donor { get; set; }
         public Doctors Doctor { get; set; }
         public DateTime DonateDate { get; set; }
         public decimal BloodQuantity { get; set; }
         public string Comment { get; set; }
         public string BloodType { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
     }
 
     public class BloodStockReportViewModel
